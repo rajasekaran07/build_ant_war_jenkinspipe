56d7382f6f250f6317b8a1d72d9f70d14313d205
Rajasekaran Radhakrishnan <rrajsekarm@gmail.com>
2016-11-23 12:53:25 +0530

    Update copy.sh

--- copy.sh
+++ copy.sh
@@ -1,2 +1,2 @@
 mkdir -p /var/lib/jenkins/test
-cp /var/lib/jenkins/workspace/build-war/build/helloworld-0.1-dev.war /var/lib/jenkins/test/
+cp /var/lib/jenkins/workspace/build-war-pipe/build/helloworld-0.1-dev.war /var/lib/jenkins/test/
